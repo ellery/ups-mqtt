23b473693355e2eed395e49a627cfeec73197d0c
Ellery J. Durgin <ellery@crowdfiber.com>
2020-09-26 04:07:35 -0400

    Remove cache and convert output to json for ease of processing attributes in Home Assistant

--- ups-mqtt.py
+++ ups-mqtt.py
@@ -9,6 +9,7 @@ from time import sleep, localtime, strftime
 import datetime
 from configparser import ConfigParser
 import shutil
+import json
 
 if not os.path.exists('conf/config.ini'):
     shutil.copy('config.ini', 'conf/config.ini')
@@ -19,12 +20,12 @@ config = ConfigParser(delimiters=('=', ), inline_comment_prefixes=('#'))
 config.optionxform = str
 config.read(config_dir)
 
-cached_values = {}
 base_topic = config['MQTT'].get('base_topic', 'home/ups')
 if not base_topic.endswith('/'):
     base_topic += '/'
 
 ups_host = config['UPS'].get('hostname', 'localhost')
+ups_name = config['UPS'].get('ups_name', 'ups')
 mqtt_host = config['MQTT'].get('hostname', 'localhost')
 mqtt_port = config['MQTT'].getint('port', 1883)
 mqtt_user = config['MQTT'].get('username', None)
@@ -32,32 +33,23 @@ mqtt_password = config['MQTT'].get('password', None)
 interval = config['General'].getint('interval', 60)
 
 def process():
-    ups = subprocess.run(["upsc", "ups@" + ups_host], stdout=subprocess.PIPE)
+    ups = subprocess.run(["upsc", ups_name  + "@" + ups_host], stdout=subprocess.PIPE)
     lines = ups.stdout.decode('utf-8').split('\n')
 
     msgs = []
-
+    json_dict = {}
     for line in lines:
         fields = line.split(':')
         if len(fields) < 2:
             continue
-
         key = fields[0].strip()
         value = fields[1].strip()
-
-        if cached_values.get(key, None) != value:
-            cached_values[key] = value
-            topic = base_topic + key.replace('.', '/').replace(' ', '_')
-            msgs.append((topic, value, 0, True))
-        
-        timestamp = time.time()
-        msgs.append((base_topic + 'timestamp', timestamp, 0, True))
-        msgs.append((base_topic + 'lastUpdate', datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S %Z'), 0, True))
-        mqtt.multiple(msgs, hostname=mqtt_host, port=mqtt_port, auth={'username': mqtt_user, 'password': mqtt_password})
+        json_dict[key] = value
+        msgs.append((base_topic + key, value, 0, True))
+    mqtt.single(base_topic + 'payload', payload=json.dumps(json_dict), hostname=mqtt_host, port=mqtt_port, auth={'username': mqtt_user, 'password': mqtt_password})
 
 
 
 while True:
     process()
     sleep(interval)
-    
